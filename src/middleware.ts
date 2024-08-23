704c81dfe9de9530ecd1e0384f18e426c9f46b84
Dana LAZAR <dana.lazar@zitec.com>
2024-08-23 16:31:16 +0300

    add login

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { jwtVerify } from 'jose';
-import { mockData } from './data/mock-data';
 
 const publicRoutes = ['/signin'];
 export const config = { matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)' };
@@ -15,28 +14,28 @@ interface Cookie {
 type DecodedUser = Cookie | null;
 
 const middleware = async (request: NextRequest) => {
-  // const currentUser = request.cookies.get('currentUser')?.value;
-  // let decodedUser: DecodedUser = null;
+  const currentUser = request.cookies.get('currentUser')?.value;
+  let decodedUser: DecodedUser = null;
 
-  // if (currentUser) {
-  //   const secret = process.env.AUTH_SECRET || '';
+  if (currentUser) {
+    const secret = process.env.AUTH_SECRET || '';
 
-  //   if (!secret) {
-  //     throw new Error('AUTH_SECRET is not defined');
-  //   }
+    if (!secret) {
+      throw new Error('AUTH_SECRET is not defined');
+    }
 
-  //   const { payload } = await jwtVerify(currentUser, new TextEncoder().encode(secret));
-  //   decodedUser = payload;
-  // }
+    const { payload } = await jwtVerify(currentUser, new TextEncoder().encode(secret));
+    decodedUser = payload;
+  }
 
-  // const currentDate = Date.now() / 1000;
+  const currentDate = Date.now() / 1000;
 
-  // if (
-  //   !publicRoutes.includes(request.nextUrl.pathname) &&
-  //   (!decodedUser || (decodedUser.exp && currentDate > decodedUser.exp))
-  // ) {
-  //   return NextResponse.redirect(new URL('/signin', request.url));
-  // }
+  if (
+    !publicRoutes.includes(request.nextUrl.pathname) &&
+    (!decodedUser || (decodedUser.exp && currentDate > decodedUser.exp))
+  ) {
+    return NextResponse.redirect(new URL('/signin', request.url));
+  }
 };
 
 export default middleware;
